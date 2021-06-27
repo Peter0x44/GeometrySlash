3cf9d132f51f027c53010201870a7ebf492082ca
Peter0x44 <peter0x44@snopyta.org>
2021-06-27 17:10:08 +0100

    Unhardcode some constants

--- src/globals.h
+++ src/globals.h
@@ -7,6 +7,14 @@
 constexpr int screenWidth = 640;
 constexpr int screenHeight = 480;
 
+constexpr int spriteSize = 48;
+constexpr int paddingSize = 6;
+
+constexpr int gridHeight = spriteSize*7 + paddingSize*8;
+constexpr int gridWidth = spriteSize*7 + paddingSize*8;
+
+constexpr int cellSize = spriteSize + paddingSize;
+
 extern Texture2D tiles;
 
 #endif
