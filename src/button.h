732b1b3a26c4412fbcce90060ab9926755d389b9
Peter0x44 <peter0x44@snopyta.org>
2021-09-01 20:17:49 +0100

    Implement start of button class

---
+++ src/button.h
@@ -0,0 +1,25 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+#include <string>
+#include <functional>
+#include <raylib.h>
+
+//typedef Rectangle (*)(void);
+
+class Button
+{
+public:
+	void draw(void);
+	bool clicked(void);
+	Button(Rectangle (*_RectCallback)(void));
+private:
+	float _fontSize;
+	std::string _text;
+	Rectangle (*_RectCallback)(void);
+	
+//	std::function<Rectangle(void)> _RectCallback;
+//	std::function<Vector2(void)> _TextPosCallback;
+//	std::function<float(void)> _FontSizeCallback;
+};
+
+#endif
--- src/resultsscreen.cpp
+++ src/resultsscreen.cpp
@@ -1,16 +1,14 @@
-#include <raylib.h>	
+#include <raylib.h>
 #include "resultsscreen.h"
-
-
-
+#include "button.h"
 
 void ResultsScreen::logic(void)
 {
-	//Determine size and position of Title Text
-	TitleFontSize = GetScreenHeight()/8;
-	TitleDims = MeasureTextEx(GetFontDefault(), "Game Over", TitleFontSize, TitleFontSize/10);
-	TitlePos.x = GetScreenWidth()/2 - TitleDims.x/2;
-	TitlePos.y = GetScreenHeight()/10 - TitleDims.y/2;
+	//Determine size and position of GameOver Text
+	GameOverFontSize = GetScreenHeight()/8;
+	GameOverDims = MeasureTextEx(GetFontDefault(), "Game Over", GameOverFontSize, GameOverFontSize/10);
+	GameOverPos.x = GetScreenWidth()/2 - GameOverDims.x/2;
+	GameOverPos.y = GetScreenHeight()/10 - GameOverDims.y/2;
 
 	//Determine size and position of Score Text
 	ScoreFontSize = GetScreenHeight()/6;
@@ -28,50 +26,79 @@ void ResultsScreen::logic(void)
 	textBox.height = MaxTextBoxDims.y + 16;
 	
 	// Code for Input box modified from Raylib example input box (https://github.com/raysan5/raylib/blob/master/examples/text/text_input_box.c)
-    	
+
 	// Get char pressed (unicode character) on the queue
-    	int key = GetCharPressed();
+	int key = GetCharPressed();
 
 	// Check if more characters have been pressed on the same frame
 	while (key > 0)
 	{
 		// NOTE: Only allow keys in range [32..125]
-                if ((key >= 32) && (key <= 125) && (letterCount < sizeof(Username) - 1))
-                {
-                    Username[letterCount] = (char)key;
-                    Username[letterCount+1] = '\0'; // Add null terminator at the end of the string.
-                    letterCount++;
-                }
+		if ((key >= 32) && (key <= 125) && (letterCount < sizeof(Username) - 1))
+		{
+			Username[letterCount] = (char)key;
+			Username[letterCount+1] = '\0'; // Add null terminator at the end of the string.
+			letterCount++;
+		}
 
-                key = GetCharPressed();  // Check next character in the queue
+		key = GetCharPressed();  // Check next character in the queue
 	}
 
-            if (IsKeyPressed(KEY_BACKSPACE))
-            {
-                letterCount--;
-                if (letterCount < 0) letterCount = 0;
-                Username[letterCount] = '\0';
-            }
+	if (IsKeyPressed(KEY_BACKSPACE))
+	{
+		letterCount--;
+		if (letterCount < 0) letterCount = 0;
+		Username[letterCount] = '\0';
+	}
 
-        framesCounter++;
-	//-------------------
+	framesCounter++;
+
+////////MainMenuButtonRect.width = PostScoreButtonRect.width = LeaderboardButtonRect.width = PlayAgainButtonRect.width = GetScreenWidth()/4.0f;
+////////MainMenuButtonRect.height = PostScoreButtonRect.height = LeaderboardButtonRect.height = PlayAgainButtonRect.height = GetScreenHeight()/7.5f;
+
+////////MainMenuButtonRect.x = PostScoreButtonRect.x = (GetScreenWidth()/3.0f)-(MainMenuButtonRect.width/2.0f);
+////////LeaderboardButtonRect.x = PlayAgainButtonRect.x = (GetScreenWidth()/3.0f)*2.0f-(MainMenuButtonRect.width/2.0f);
+
+////////MainMenuButtonRect.y = PlayAgainButtonRect.y = (GetScreenHeight()/6.0f)*4-(MainMenuButtonRect.height/2.0f);
+////////LeaderboardButtonRect.y = PostScoreButtonRect.y = ((GetScreenHeight()/6.0f)*5.0f)-(MainMenuButtonRect.height/2.0f);
 
 
-	
 }
+
+Rectangle MainMenuButtonRectCallback(void)
+{
+        Rectangle tmp;
+        tmp.width = GetScreenWidth()/4.0f;
+        tmp.height = GetScreenHeight()/7.5f;
+        tmp.x = (GetScreenWidth()/3.0f)-(tmp.width/2.0f);
+        tmp.y = (GetScreenHeight()/6.0f)*4-(tmp.height/2.0f);
+        return tmp;
+}
+
 
 void ResultsScreen::render(void)
 {
-	DrawText("Game Over", TitlePos.x, TitlePos.y, TitleFontSize, MAROON);
+	DrawText("Game Over", GameOverPos.x, GameOverPos.y, GameOverFontSize, MAROON);
 	DrawText(TextFormat("%d", score), ScorePos.x, ScorePos.y, ScoreFontSize, BLACK);
 	DrawRectangleRounded(textBox, 0.5f, 0, LIGHTGRAY);
-        DrawRectangleRoundedLines(textBox, 0.5f, 0, 2.0f, MAROON);
+	DrawRectangleRoundedLines(textBox, 0.5f, 0, 2.0f, MAROON);
 
-        DrawText(Username, textBox.x + 5, textBox.y + 8, UsernameFontSize, MAROON);
+	DrawText(Username, textBox.x + 5, textBox.y + 8, UsernameFontSize, MAROON);
 
-        if (letterCount < sizeof(Username) - 1)
+	if (letterCount < sizeof(Username) - 1)
 	{
 		// Draw blinking underscore char
 		if (((framesCounter/30)%2) == 0) DrawText("_", textBox.x + 8 + MeasureText(Username, UsernameFontSize), textBox.y + 12, UsernameFontSize, MAROON);
 	}
+	MainMenuButton->draw();
+////////DrawRectangleRec(MainMenuButtonRect, LIGHTGRAY);
+////////DrawRectangleRec(PostScoreButtonRect, LIGHTGRAY);
+////////DrawRectangleRec(LeaderboardButtonRect, LIGHTGRAY);
+////////DrawRectangleRec(PlayAgainButtonRect, LIGHTGRAY);
+}
+
+
+ResultsScreen::ResultsScreen(void)
+{
+	MainMenuButton = new Button(&MainMenuButtonRectCallback);
 }
