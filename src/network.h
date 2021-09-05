54ba05d65b2a3512158f070236aeace130f32025
Peter0x44 <peter0x44@snopyta.org>
2021-09-05 12:07:39 +0100

    Remove unused struct

--- src/network.h
+++ src/network.h
@@ -1,7 +1,6 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
-#include <cstddef>
 #include <string>
 
 void PostScores(void);
@@ -9,11 +8,5 @@ std::string& GetScores(void);
 
 constexpr const char* PostEndpoint = "http://localhost:3000/submit";
 constexpr const char* GetEndpoint = "http://localhost:3000/top";
-
-struct MemoryStruct
-{
-	char *memory;
-	size_t size;
-};
 
 #endif
